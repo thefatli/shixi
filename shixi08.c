fb8e37380b29ce8a4617e7b2423b6d3da4f37a3d
thefatli <3105340836@qq.com>
2021-10-20 09:20:51 +0800

    shixi

---
+++ shixi08.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int max=0,min=0,sum=0,n;
+	while(scanf("%d ",&n)!=EOF){
+		int a[n];
+		for(int i=0;i<n;i++){
+			scanf("%d ",&a[i]);
+			sum+=a[i];
+		}for(int j=0;j<n;j++){
+			if(a[j]<min){
+				min=a[j];
+			}if(a[j]>max){
+				max=a[j];
+				}}
+		double k;
+		k=(sum-max-min)/(n-2);
+		printf("%.2lf ",k);
+	}return 0;
+}
